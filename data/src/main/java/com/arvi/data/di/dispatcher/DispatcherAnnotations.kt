14e8db249630a02bcbcce91c7f9474b12c48c050
Mohamad Wildan Garviandi <garviandiwildan@gmail.com>
2024-05-19 16:00:01 +0100

    add missing file dispatcher module

---
+++ data/src/main/java/com/arvi/data/di/dispatcher/DispatcherAnnotations.kt
@@ -0,0 +1,11 @@
+package com.arvi.data.di.dispatcher
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class IoDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class DefaultDispatcher
